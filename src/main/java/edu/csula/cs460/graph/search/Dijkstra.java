795381820a25d10a9d5919877850f252be273b57
Daniel Martinez <dryanmartinez@yahoo.com>
2015-11-14 18:16:29 -0800

    Started on Dijkstra's Part

--- src/main/java/edu/csula/cs460/graph/search/Dijkstra.java
+++ src/main/java/edu/csula/cs460/graph/search/Dijkstra.java
@@ -4,11 +4,29 @@ import edu.csula.cs460.graph.Edge;
 import edu.csula.cs460.graph.Graph;
 import edu.csula.cs460.graph.Node;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Dijkstra implements SearchStrategy {
     @Override
     public List<Edge> search(Graph graph, Node source, Node dist) {
+//    	List<Node> visitedNodes;
+//    	Queue<Node> queue = new LinkedList<>();
+    	PriorityQueue<Map<Node, Integer>> pq = new PriorityQueue<>();
+    	Map<Node, Integer> nodeValues = new HashMap<Node,Integer>();
+    	
+    	nodeValues.put(source, 0);
+    	
+    	
+    	while(!pq.isEmpty()){
+    		
+    	}
+    	
+    	
         return null;
     }
 }
